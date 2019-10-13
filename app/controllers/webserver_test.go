e862c06d7c94facfcc62c23582523d4cd46e453d
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-10-13 14:58:23 +0900

    Improve webserver test code to use easyer way

--- app/controllers/webserver_test.go
+++ app/controllers/webserver_test.go
@@ -10,27 +10,21 @@ import (
 	"testing"
 )
 
+func TestFirstThree(t *testing.T) {
+
+}
+
 func TestViewHRTFHandler(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(viewHRTFHandler))
 	defer ts.Close()
 	t.Logf("test ViewHRTFHandler")
 
-	//req := httptest.NewRequest("GET", ts.URL+"/hrtf/?id=1", nil)
-	req := httptest.NewRequest("GET", ts.URL+"/hrtf/", nil)
-	q := req.URL.Query()
-	q.Add("id", "1")
-	req.URL.RawQuery = q.Encode()
+	req := httptest.NewRequest("GET", ts.URL+"/hrtf/?id=1", nil)
+	res := httptest.NewRecorder()
+	viewHRTFHandler(res, req)
 
-	req.RequestURI = ""
-
-	c := http.DefaultClient
-
-	t.Logf("log. header: %v", req.Header)
-	t.Logf("log. body: %v", req.Body)
-	t.Logf("log. requestURI: %v", req.RequestURI)
-	res, err := c.Do(req)
-	if err != nil {
-		t.Fatalf("Error by http.Get(). %v", err)
+	if res.Code != http.StatusOK{
+		t.Errorf("invalid code. want %v, got %v", http.StatusOK, res.Code)
 	}
 
 	data, err := ioutil.ReadAll(res.Body)
@@ -38,10 +32,6 @@ func TestViewHRTFHandler(t *testing.T) {
 		t.Fatalf("Error by ioutil.ReadAll(). %v", err)
 	}
 	t.Log(data)
-
-	if status := res.StatusCode; status != http.StatusOK {
-		t.Errorf("Error by res.StatusCode. want %v, got %v", http.StatusOK, status)
-	}
 
 	tpls := template.Must(template.New("").Funcs(fm).ParseFiles("app/views/hrtf.html", "app/views/analysis.html"))
 	hrtf, err := models.GetHRTF("1")
