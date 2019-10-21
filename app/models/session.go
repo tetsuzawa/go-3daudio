10b8cfba3085975acec36479edaf2f2a45669799
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-10-21 12:36:44 +0900

    Fix session key

--- app/models/session.go
+++ app/models/session.go
@@ -5,70 +5,50 @@ import (
 	"log"
 )
 
-type User struct {
-	ID        string `json:"id"`
-	UserName  string `json:"username"`
-	Password  string `json:"password"`
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
+type Session struct {
+	SessionID string `json:"session_id"`
+	UserID    string `json:"user_id"`
 }
 
-func NewUser(id, username, password, firstname, lastname string) *User {
-	return &User{
-		ID:        id,
-		UserName:  username,
-		Password:  password,
-		FirstName: firstname,
-		LastName:  lastname,
+func NewSession(sessionId, userId string) *Session {
+	return &Session{
+		SessionID: sessionId,
+		UserID:    userId,
 	}
 }
 
-func (u *User) TableName() string {
-	return GetUserTableName("user")
+func (s *Session) TableName() string {
+	return GetSessionTableName("session")
 }
 
-func (u *User) Create() error {
-	cmd := fmt.Sprintf("INSERT INTO %s (id, username, password, firstname, lastname) VALUES (?, ?, ?, ?, ?)", u.TableName())
-	_, err := DbConnection.Exec(cmd, u.ID, u.UserName, u.Password, u.FirstName, u.LastName)
+func (s *Session) Create() error {
+	cmd := fmt.Sprintf("INSERT INTO %s (sessionid, userid) VALUES (?, ?)", s.TableName())
+	_, err := DbConnection.Exec(cmd, s.SessionID, s.UserID)
 	if err != nil {
 		return err
 	}
 	return err
 }
 
-func (u *User) Save() error {
-	cmd := fmt.Sprintf("UPDATE %s SET username = ?, password = ?, firstname = ?, lastname = ? WHERE id = ?", u.TableName())
-	_, err := DbConnection.Exec(cmd, u.UserName, u.Password, u.FirstName, u.LastName, u.ID)
+func (s *Session) Save() error {
+	cmd := fmt.Sprintf("UPDATE %s SET userid = ? WHERE sessionid = ?", s.TableName())
+	_, err := DbConnection.Exec(cmd, s.UserID, s.SessionID)
 	if err != nil {
 		return err
 	}
 	return err
 }
 
-func GetUser(id string) (*User, error) {
+func GetSession(sessionID string) (*Session, error) {
 	tableName := GetUserTableName("user")
-	cmd := fmt.Sprintf(`SELECT id, username, password, firstname, lastname FROM %s WHERE id = '%s'`,
-		tableName, id)
+	cmd := fmt.Sprintf(`SELECT sessionid, userid FROM %s WHERE sessionID = '%s'`,
+		tableName, sessionID)
 	row := DbConnection.QueryRow(cmd)
-	var u User
-	err := row.Scan(&u.ID, &u.UserName, &u.Password, &u.FirstName, &u.LastName)
+	var s Session
+	err := row.Scan(&s.SessionID, &s.UserID)
 	if err != nil {
 		log.Println(err)
 		return nil, err
 	}
-	return NewUser(u.ID, u.UserName, u.Password, u.FirstName, u.LastName), nil
-}
-
-func GetUserByUserName(un string) (*User, error) {
-	tableName := GetUserTableName("user")
-	cmd := fmt.Sprintf(`SELECT id, username, password, firstname, lastname FROM %s WHERE username = '%s'`,
-		tableName, un)
-	row := DbConnection.QueryRow(cmd)
-	var u User
-	err := row.Scan(&u.ID, &u.UserName, &u.Password, &u.FirstName, &u.LastName)
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-	return NewUser(u.ID, u.UserName, u.Password, u.FirstName, u.LastName), nil
+	return NewSession(s.SessionID, s.UserID), nil
 }
