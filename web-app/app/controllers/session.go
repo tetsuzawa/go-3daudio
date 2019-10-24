cff3bd255b7f6aae3667a325d83d93382d9b11d6
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-10-25 07:53:29 +0900

    Change directory structure

---
+++ web-app/app/controllers/session.go
@@ -0,0 +1,77 @@
+package controllers
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/tetsuzawa/go-3daudio/web-app/app/models"
+)
+
+var dbSessionsCleaned time.Time
+
+func getUser(w http.ResponseWriter, r *http.Request) models.User {
+	// get cookie
+	c, err := r.Cookie("session")
+	if err != nil {
+		sID, _ := uuid.NewV4()
+		c = &http.Cookie{
+			Name:     "session",
+			Value:    sID.String(),
+			Path:     "/",
+			HttpOnly: true,
+		}
+	}
+	http.SetCookie(w, c)
+
+	// if the user exists already, get user
+	var u *models.User
+
+	s, err := models.GetSession(c.Value)
+	if err == nil {
+		u, err = models.GetUserByUserName(s.UserName)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return *u
+	}
+	return models.User{}
+}
+
+func alreadyLoggedIn(r *http.Request) bool {
+	c, err := r.Cookie("session")
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	s, err := models.GetSession(c.Value)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	_, err = models.GetUserByUserName(s.UserName)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
+}
+
+func cleanSessions() {
+	t := time.Now()
+	t.Add(-30 * time.Second)
+	ss, err := models.GetOldSessions(t)
+	if err != nil {
+		log.Println(err)
+	}
+	dbSessionsCleaned = time.Now()
+
+	for _, s := range ss {
+		if err := s.Delete(); err != nil {
+			log.Println(err)
+		}
+	}
+}
---
+++ web-app/app/controllers/webserver_test.go
@@ -0,0 +1,76 @@
+package controllers
+
+import (
+	"bytes"
+	models2 "github.com/tetsuzawa/go-3daudio/web-app/app/models"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestFirstThree(t *testing.T) {
+	patterns := []struct {
+		want  string
+		given string
+	}{
+		{"abc", "abcde"},
+		{"Abc", "Abcde"},
+		{"012", "01234"},
+	}
+	for idx, p := range patterns {
+		got := firstThree(p.given)
+
+		if p.want != got {
+			t.Errorf("Case(%d) want %v, got %v", idx, p.want, got)
+		}
+	}
+}
+
+func TestViewHRTFHandler(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(viewHRTFHandler))
+	defer ts.Close()
+	t.Logf("test ViewHRTFHandler")
+
+	req := httptest.NewRequest("GET", ts.URL+"/hrtf/?id=1", nil)
+	res := httptest.NewRecorder()
+	viewHRTFHandler(res, req)
+
+	if res.Code != http.StatusOK {
+		t.Errorf("invalid code. want %v, got %v", http.StatusOK, res.Code)
+	}
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("Error by ioutil.ReadAll(). %v", err)
+	}
+	t.Log(data)
+
+	tpls := template.Must(template.New("").Funcs(fm).ParseFiles("app/views/templates/hrtf.html", "app/views/templates/analysis.html"))
+	hrtf, err := models2.GetHRTF("1")
+	if err != nil {
+		t.Fatalf("Error by models.GetHRTF(). %v", err)
+	}
+
+	var buff = new(bytes.Buffer)
+
+	err = tpls.ExecuteTemplate(buff, "hrtf.html", hrtf)
+	if err != nil {
+		t.Fatalf("Error by tpls.ExecuteTemplate(). %v", err)
+	}
+
+	want := buff.String()
+
+	if string(data) != want {
+		t.Errorf("Error by rr.Body.String().\n \nwant \n%v\n,\n \ngot \n%v", want, string(data))
+	}
+}
+
+func TestApiMakeHandler(t *testing.T) {
+	hf := apiMakeHandler(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+	//TODO
+
+}
