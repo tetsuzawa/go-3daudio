c9d9435e8c07c58b7c23f595625410a80c3e4beb
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-12-12 19:33:11 +0900

    Add Create()

--- web-app/app/models/user.go
+++ web-app/app/models/user.go
@@ -2,8 +2,14 @@ package models
 
 import (
 	"context"
+	"fmt"
 	"github.com/pkg/errors"
+	userpb "github.com/tetsuzawa/go-3daudio/web-app/proto/user"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
 )
 
 type User struct {
@@ -15,8 +21,10 @@ type User struct {
 	Role      string `json:"role" bson:"role"`
 }
 
-func NewUser(id, username, password, firstname, lastname, role string) *User {
-	return &User{
+type UserServicer struct{}
+
+func NewUser(id, username, password, firstname, lastname, role string) *UserServicer {
+	return &UserServicer{
 		ID:        id,
 		UserName:  username,
 		Password:  password,
@@ -26,46 +34,72 @@ func NewUser(id, username, password, firstname, lastname, role string) *User {
 	}
 }
 
-func (u *User) TableName() string {
+func (u *UserServicer) TableName() string {
 	return GetTableName(tableNameUserData)
 }
 
-func (u *User) Create() error {
+func (u *UserServicer) Create(ctx context.Context, req *userpb.CreateUserReq) (*userpb.CreateUserRes, error) {
 	userCollection := db.Collection(u.TableName())
 
+	user := req.GetUser()
+
+	data := User{
+		//ID:        user.Id,  //empty, Mongodb generates a unique object ID
+		UserName:  user.GetUserName(),
+		Password:  user.GetPassword(),
+		FirstName: user.GetFirstName(),
+		LastName:  user.GetLastName(),
+		Role:      user.GetRole(),
+	}
+
+	//b, err := bson.Marshal(data)
+	//if err != nil {
+	//	log.Printf("failed to encode at bson.Marshal(): %v\n", err)
+	//	return nil, status.Errorf(codes.Internal, fmt.Sprintf("Internal error: %v", err))
+	//}
+	result, err := userCollection.InsertOne(context.TODO(), data)
+	if err != nil {
+		// return internal gRPC error to be handled later
+		//return errors.Wrap(err, "failed to insert data at InsertOne()")
+		log.Printf("failed to insert document at InsertOne: %v\n", err)
+		return nil, status.Errorf(codes.Internal, fmt.Sprintf("Internal error: %v", err))
+	}
+
+	// add the id to user, first cast the "generic type" (go doesn't have real generics yet) to an Object ID.
+	old := result.InsertedID.(primitive.ObjectID)
+	//	// Convert the object id to it's string counterpart
+	user.Id = old.Hex()
+	return &userpb.CreateUserRes{User: user}, nil
+}
+
+func (u *UserServicer) Read(ctx context.Context, req *userpb.ReadUserReq) (*userpb.ReadUserRes, error) {
+	userCollection := db.Collection(u.TableName())
+
+	id := req.GetId()
+
+	filter := bson.D{{"id", id}}
 	b, err := bson.Marshal(u)
 	if err != nil {
 		return errors.Wrap(err, "failed to encode at bson.Marshal()")
 	}
-	_, err = userCollection.InsertOne(context.TODO(), b)
-	if err != nil {
-		return errors.Wrap(err, "failed to insert data at InsertOne()")
-	}
 
-	return nil
+	var u User
+	err := userCollection.FindOne(context.TODO(), filter).Decode(&u)
+
 }
 
-func (u *User) Save() error {
-	userCollection := db.Collection(u.TableName())
-
-	filter := bson.D{{"id", u.ID}}
-	b, err := bson.Marshal(u)
-	if err != nil {
-		return errors.Wrap(err, "failed to encode at bson.Marshal()")
-	}
-	_, err = userCollection.UpdateOne(context.TODO(), filter, b)
-	if err != nil {
-		return errors.Wrap(err, "failed to insert data at InsertOne()")
-	}
-	return nil
-}
-
-func GetUser(id string) (*User, error) {
+//_, err = userCollection.UpdateOne(context.TODO(), filter, id)
+//if err != nil {
+//return errors.Wrap(err, "failed to insert data at InsertOne()")
+//}
+//return nil
+//
+func GetUser(id string) (*UserServicer, error) {
 	userCollection := db.Collection(GetTableName(tableNameUserData))
 
 	filter := bson.D{{"id", id}}
 
-	var u User
+	var u UserServicer
 	err := userCollection.FindOne(context.TODO(), filter).Decode(&u)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to find data at FindOne()")
@@ -73,12 +107,12 @@ func GetUser(id string) (*User, error) {
 	return NewUser(u.ID, u.UserName, u.Password, u.FirstName, u.LastName, u.Role), nil
 }
 
-func GetUserByUserName(un string) (*User, error) {
+func GetUserByUserName(un string) (*UserServicer, error) {
 	userCollection := db.Collection(GetTableName(tableNameUserData))
 
 	filter := bson.D{{"user_name", un}}
 
-	var u User
+	var u UserServicer
 	err := userCollection.FindOne(context.TODO(), filter).Decode(&u)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to find data at FindOne()")
